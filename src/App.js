3aa6399b1c173e79fd9132c10cf06efbb6e0d339
Bae-Jin-Young <bae20051018@gmail.com>
2021-12-29 20:16:36 +0900

    feat: searchbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React, { useState } from "react";
 import MainWindow from "./components/MainWindow";
+import SearchBar from "./components/SearchBar";
 import Subform from "./components/Subform";
+import TItleAndSearch from "./components/TItleAndSearch";
 
 const App = () => {
   return (
-    <div
-      style={{
-        width: "100%",
-        height: "100%",
-        background: "#111111",
-        display: "flex",
-        justifyContent: "space-around",
-        alignItems: "center",
-      }}
-    >
-      <MainWindow />
-      <Subform />
+    <div>
+      <div
+        style={{
+          width: "100%",
+          height: "100vh",
+          background: "#111111",
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+        }}
+      >
+        <MainWindow />
+        <Subform />
+      </div>
+      <div
+        style={{
+          width: "100%",
+          height: "100vh",
+          background: "#111111",
+        }}
+      >
+        <TItleAndSearch />
+      </div>
     </div>
   );
 };
---
+++ src/components/TItleAndSearch.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+import "../styles/searchBar.css";
+import { useState } from "react/cjs/react.development";
+
+const TItleAndSearch = () => {
+  const [state, setState] = useState(false);
+  const [text, setText] = useState("");
+  const onClick = () => {
+    setState(!state);
+    setText("");
+  };
+  const onChangeText = (e) => {
+    setText(e.target.value);
+  };
+  return (
+    <div className="outtitlensearch">
+      <div className="ctitle">
+        <h1>lessf!ow 컬렉션</h1>
+        <p>다른 사람이 만든 컬렉션을 살펴보세요</p>
+      </div>
+      <div className="titlensearch">
+        <div>
+          {/* <SearchBar /> */}
+          <div className={`search ${state ? "active" : ""}`}>
+            <div className="icon" onClick={onClick}></div>
+            <div className="input">
+              <input
+                type="text"
+                placeholder="Search"
+                value={text}
+                id="mysearch"
+                onChange={onChangeText}
+              />
+            </div>
+            <span className="clear" onClick={onClick}></span>
+          </div>
+          {/* <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            const icon = document.querySelector('.icon');
+            const search = document.querySelector('.search');
+            icon.onclick = function(){
+                search.classList.toggle('active');
+            }
+        `,
+          }}
+        ></script> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TItleAndSearch;
